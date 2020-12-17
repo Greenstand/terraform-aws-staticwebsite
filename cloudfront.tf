a8f8160c1396dfd0549d283ba8e0e90863f2ccd1
Zaven <matthew.fullstackgis@gmail.com>
2020-12-16 18:52:17 -0800

    Create and try to use a cert

--- cloudfront.tf
+++ cloudfront.tf
@@ -50,12 +50,8 @@ resource "aws_cloudfront_distribution" "main" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-/*  viewer_certificate {
     acm_certificate_arn      = "${aws_acm_certificate_validation.cert.certificate_arn}"
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
-  }*/
+  }
 }
